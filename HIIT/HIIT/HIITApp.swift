8027ecbe5a150d2272c930488444ba5611418c4e
pakang2 <123673517+pakang2@users.noreply.github.com>
2025-02-18 09:21:40 -0500

    first commit

---
+++ HIIT/HIIT/HIITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITApp.swift
+//  HIIT
+//
+//  Created by Paul Kang on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
